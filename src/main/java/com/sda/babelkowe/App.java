92151e5291d3be8871cd16d9cd1eeea6aa93164b
Szymon Luniewski <sizmon15@gmail.com>
2019-05-19 12:17:59 +0100

    Sortowanie Tabela d

--- src/main/java/com/sda/babelkowe/App.java
+++ src/main/java/com/sda/babelkowe/App.java
@@ -1,13 +1,51 @@
 package com.sda.babelkowe;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static int[] sort(int[] tab) {
+        for (int j = 0; j < tab.length; j++) {
+
+            for (int i = 0; i < tab.length - 1; i++) {
+                if (tab[i] > tab[i + 1]) {
+                    int tmp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = tmp;
+                }
+            }
+        }
+            System.out.println(Arrays.toString(tab));
+            return tab;
+
+
+        }
+
+
+        public static void main (String[]args )
+        {
+            Random random = new Random();
+            random.nextInt();
+            int max = 5000;
+            int min = -5000;
+            int[] tab = new int[20];
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < tab.length; i++) {
+                int randomNumber = random.nextInt(max - min + 1) + min;
+                tab[i] = randomNumber;
+
+
+            }
+            long endTime = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+            System.out.println("Czas wykonywania:" + totalTime);
+        }
+
+
     }
-}
+
+
